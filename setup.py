451e2fff59a9762dba9ca8d2610906b3d82842c0
Ben Appleby <ben.appleby@sky.com>
2020-09-22 01:13:20 +0100

    prepare for pip

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py-iss-telemetry", # Replace with your own username
+    version="1.0",
+    author="Ben Appleby",
+    author_email="ben.appleby@sky.com",
+    description="Stream live International Space Station Telemetry.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BApplB/py-iss-telemetry",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache 2.0 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
