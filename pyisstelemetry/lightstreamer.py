2ad021d06b9527e99ff5e5326d7bfa384f2cc715
BApplB <56032382+BApplB@users.noreply.github.com>
2025-03-02 23:57:07 +0000

    Added flake8 exclusion to lightstreamer.py

--- pyisstelemetry/lightstreamer.py
+++ pyisstelemetry/lightstreamer.py
@@ -14,6 +14,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+# flake8: noqa
+
 __copyright__ = "Copyright Lightstreamer Srl."
 
 import sys
